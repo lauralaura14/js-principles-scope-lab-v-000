06c1fd27c6b460a0994c0264d00403de98501b89
Laura <laurao148@gmail.com>
2018-07-31 04:18:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var customerName;
+
 function upperCaseCustomerName(customerName) {
   return customerName.toUpperCase();
+}
+
+function setBestCustomer(bestCustomer) {
+  var bestCustomer = 'not bob';
+  return bestCustomer;
 }
